f331d17b369e3527ec1e9765e8ac24689ccefcbb
yangdefei <defei.yang@fiot-lab.org>
2019-04-24 10:47:57 +0800

    update

---
+++ src/main/java/zxm/config/CorsConfig.java
@@ -0,0 +1,41 @@
+package zxm.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * 解决跨域访问问题
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*"); // 2
+        corsConfiguration.addAllowedMethod("*"); // 3
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //2M
+        factory.setMaxFileSize(DataSize.ofBytes(2 * 1024L * 1024L));
+        return factory.createMultipartConfig();
+    }
+}
--- src/main/java/zxm/occore/constant/OCConstant.java
+++ src/main/java/zxm/occore/constant/OCConstant.java
@@ -14,25 +14,7 @@ public class OCConstant {
         BIND_DEVICE("bindDevice"),//绑定设备
         DEVICE_ADDED("deviceAdded"),//添加新设备
         DEVICE_INFO_CHANGED("deviceInfoChanged"),//设备信息变化
-        DEVICE_DATA_CHANGED("deviceDataChanged"),//设备数据变化
-        DEVICE_DATAS_CHANGED("deviceDatasChanged"),//设备数据批量变化
-        DEVICE_DELETED("deviceDeleted"),//删除设备
-        MESSAGE_CONFIRM("messageConfirm"),//消息确认
-        COMMAND_RSP("commandRsp"),//命令响应
-        DEVICE_EVENT("deviceEvent"),//设备事件
-        APP_DELETED("appDeleted"),//应用删除
-        SERVICE_INFO_CHANGED("serviceInfoChanged"),//服务信息变化
-        RULE_EVENT("ruleEvent"),//规则事件
-        LOCATION_CHANGED("locationChanged"),//位置变化
-        DEVICE_MODEL_ADDED("deviceModelAdded"),//添加设备模型
-        DEVICE_MODEL_DELETED("deviceModelDeleted"),//删除设备模型
-        VEHICLE_INFO_CHANGED("vehicleInfoChanged"),//车辆信息变更
-        DEVICE_ALARM_CHANGED("deviceAlarmChanged"),//设备告警变更
-        DEVICE_REALTIME_DATA_QUERY_STATUS_CHANGED("deviceRealTimeDataQueryStatusChanged"),//获取设备实时数据状态变更
-        DEVICE_DESIRED_PROPERTIES_MODIFY_STATUS_CHANGED("deviceDesiredPropertiesModifyStatusChanged"),//设备影子状态变更
-        APP_AUTHORIZATION("appAuthorization"),//应用授权通知
         APP_DEAUTHORIZATION("appDeauthorization");//应用取消授权通知
-
         String name;
 
         NotifyType(String name) {
